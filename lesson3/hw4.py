64174a4b9eef421535f4af7245aa5ab1f8339208
Maks <74023131+MaksymKhatskevych@users.noreply.github.com>
2021-01-11 20:50:23 +0200

    Update hw4.py

--- lesson3/hw4.py
+++ lesson3/hw4.py
@@ -6,7 +6,9 @@ path = sys.argv[1]
 print(f"Start in {path}")
 
 # files - это список имен файлов и папок в path.
+
 files = os.listdir(path)
+
 
 music = []
 images = []
@@ -21,28 +23,37 @@ image_file_format = ('JPEG', 'PNG', 'JPG')
 video_file_format = ('AVI', 'MP4', 'MOV')
 documents_file_format = ('DOC', 'DOCX', 'TXT')
 
-
-for file in files:
-    if file.endswith('.mp3') or file.endswith('ogg') or file.endswith('.wav') or file.endswith('.amr'):
-        music.append(file)          
-    elif file.endswith('.jpeg') or file.endswith('.png') or file.endswith('.jpg'):
-        images.append(file)
-    elif file.endswith('.avi') or file.endswith('.mp4') or file.endswith('.mov'):
-        video.append(file)
-    elif file.endswith('.doc') or file.endswith('.docx') or file.endswith('.txt'):
-        documents.append(file)
-    else:
-        other_files.append(file)              
-
-    
-
-
-   
-    
 print(f"Base directory {files}")
-print("_____________________________________")
+print("_____________________________________") 
+
+
+audio_file_format = [(el.lower()) for el in audio_file_format]
+image_file_format = [(el.lower()) for el in image_file_format]
+video_file_format = [(el.lower()) for el in video_file_format]
+documents_file_format = [(el.lower()) for el in documents_file_format]
+
+
+    
+for file in files:
+    
+    if file.rsplit('.')[-1] in audio_file_format:
+        music.append(file)
+        
+    elif file.rsplit('.')[-1] in image_file_format:
+        images.append(file)
+        
+    elif file.rsplit('.')[-1] in video_file_format:
+        video.append(file)
+        
+    elif file.rsplit('.')[-1] in documents_file_format:
+        documents.append(file)
+        
+    else:
+        other_files.append(file)        
+
 print(f"music: {music}")
 print(f"images: {images}")
 print(f"video: {video}")
 print(f"documents: {documents}") 
-print(f"other_files: {other_files}") 	          
+print(f"other_files: {other_files}")             
+  
