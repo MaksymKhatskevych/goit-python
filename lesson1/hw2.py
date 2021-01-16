848e7f8ac7c394ef9b082338b99efeb4f5856308
Maks <74023131+MaksymKhatskevych@users.noreply.github.com>
2021-01-16 14:34:08 +0200

    Update hw2.py

--- lesson1/hw2.py
+++ lesson1/hw2.py
@@ -5,7 +5,7 @@ b = float(input("b = "))
 c = float(input("c = "))
 
 D = b ** 2 - 4 * a * c
-print("Дискриминант D : ",+ float(D))
+print("Дискриминант D : ", float(D))
 
 
 x1 = (-b + D**0.5) / 2*a
@@ -16,9 +16,3 @@ x2 = (-b - D**0.5) / 2*a
 print("x1 = ", float(x1))
 print("x2 = ", float(x2))
 
-#D = b2 - 4a
-#x1 = (-b + D0.5) / 2a
-
-#x2 = (-b - D0.5) / 2a
-
-#ax2 + bx + c = 0
