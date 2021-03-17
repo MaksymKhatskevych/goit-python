808729e7c8251bf16182d353c623a122461bbf74
Maks <74023131+MaksymKhatskevych@users.noreply.github.com>
2021-03-18 00:57:24 +0200

    Delete lesson7/clean_folder directory

--- lesson7/clean_folder/clean_folder/__init__.py
+++
@@ -1,8 +0,0 @@
-import os
-from pathlib import Path
-import re
-import sys
-import shutil
-
-
-__all__ = ['normalize','sort','main']
--- lesson7/clean_folder/setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import find_packages
-
-
-setup(
-    name = "clean_folder",
-    version = "1.0",
-    packages = "find_packages",
-)
