f1fdfa2c96a28284169b3b748ee50a108e7503c3
Maks <74023131+MaksymKhatskevych@users.noreply.github.com>
2021-03-17 23:51:54 +0200

    Add files via upload

---
+++ lesson7/clean_folder/setup.py
@@ -0,0 +1,14 @@
+from setuptools import find_packages
+import os
+from pathlib import Path
+import re
+import sys
+import shutil
+
+
+
+setup(
+    name = "clean_folder",
+    version = "1.0",
+    packages = "find_packages",
+)
